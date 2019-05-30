c91116dbb62c6555c604a27170d100ceecad2667
Franklin Barreto <franklin-barreto@hotmail.com>
2019-05-30 11:46:07 +0100

    add ServiceContainerInterface

---
+++ src/ServiceContainerInterface.php
@@ -0,0 +1,10 @@
+<?php
+namespace FINANCE;
+
+interface ServiceContainerInterface
+{
+    public function add(string $name, $service);
+    public function addLazy(string $name, callable $callable);
+    public function get(string $name);
+    public function has(string $name);
+}
